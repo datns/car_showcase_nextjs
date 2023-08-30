ee71dc47d5f2ef982b37c837484652368cdc8edc
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-08-30 16:05:18 +0700

    create CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,13 +3,14 @@ import {useState} from "react";
 import Image from "next/image";
 import {CarProps} from "@/types";
 import {calculateCarRent} from "@/utils";
-import {CustomButton} from "@/components/index";
+import {CarDetails, CustomButton} from "@/components/index";
 
 interface CarCardProps {
 	car: CarProps
 }
 
 const CarCard = ({car}: CarCardProps) => {
+	const [isOpen, setIsOpen] = useState(false);
 	const { city_mpg,year, make, model, transmission, drive } = car;
 
 	const carRent = calculateCarRent(city_mpg, year);
@@ -65,11 +66,12 @@ const CarCard = ({car}: CarCardProps) => {
 						containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
 						textStyles="text-white text-[14px] leading-[17px] font-bold"
 						rightIcon="/right-arrow.svg"
-						handleClick={() => {}}
+						handleClick={() => setIsOpen(true)}
 					/>
 				</div>
 			</div>
 
+			<CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
 		</div>
 	)
 }
--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export {default as Navbar} from './Navbar';
 export {default as Footer} from './Footer';
 export {default as SearchBar} from './SearchBar';
 export {default as CarCard} from './CarCard';
+export {default as CarDetails} from './CarDetails';
