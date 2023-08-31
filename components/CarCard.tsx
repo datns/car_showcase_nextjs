ab219dc88fde533089a674df5fb1a1390d37fb84
dat.s.nguyen <dat.s.snguyen@taptap.com.vn>
2023-08-31 21:28:33 +0700

    handle image in CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,7 +2,7 @@
 import {useState} from "react";
 import Image from "next/image";
 import {CarProps} from "@/types";
-import {calculateCarRent} from "@/utils";
+import {calculateCarRent, generateCarImageUrl} from "@/utils";
 import {CarDetails, CustomButton} from "@/components/index";
 
 interface CarCardProps {
@@ -17,7 +17,7 @@ const CarCard = ({car}: CarCardProps) => {
 	return (
 		<div className="car-card group">
 			<div className="car-card__content">
-				<h2>
+				<h2 className="car-card__content-title">
 					{make} {model}
 				</h2>
 			</div>
@@ -33,7 +33,7 @@ const CarCard = ({car}: CarCardProps) => {
 			</p>
 
 			<div className="relative w-full h-40 my-3 object-contain">
-				<Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+				<Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
 			</div>
 
 			<div className="relative flex w-full mt-2">
